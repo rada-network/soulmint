1841e20af8dc42a9b452ec8579147a9ab08ba1c1
Qvv <quynhvv@joomsolutions.com>
2022-10-11 13:11:59 +0700

    Clean

--- src/hooks/Uploader/useUploader.js
+++
@@ -1,8 +0,0 @@
-// import { useCallback, useState } from 'react';
-
-export default (props) => {
-
-  return {
-
-  };
-};
--- src/utils/getLibrary.js
+++
@@ -1,5 +0,0 @@
-import { ethers } from 'ethers'
-export const getLibrary = (provider) => {
-  const library = new ethers.providers.Web3Provider(provider)
-  return library
-}
